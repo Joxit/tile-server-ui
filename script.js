1032e7a79070d838f0991cce464f39a85c01760d
Joxit <Joxit972@gmail.com>
2016-04-05 22:56:34 +0200

    Add button and dialog for adding tile server url

--- script.js
+++ script.js
@@ -14,18 +14,59 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-var mymap = L.map('map').setView([ 49, 1.2 ], 13);
+var mymap = L.map('map', {zoomControl: true}).setView([ 49, 1.2 ], 13);
 var tileServer = JSON.parse(localStorage.getItem('tileServer'))[0];
-L.tileLayer(tileServer, {
+var opts = {
   attribution: '&copy; <a href="https://github.com/Joxit">Joxit</a> and your tile server',
   maxZoom: 22,
-}).addTo(mymap);
+};
+L.tileLayer(tileServer, opts).addTo(mymap);
+
+var AddButton = L.Control.extend({
+  options: {
+    position: 'bottomright'
+  },
+
+  onAdd: function (map) {
+    // create the control container with a particular class name
+    var button = L.DomUtil.create('button',
+        'mdl-button mdl-js-button mdl-button--fab mdl-button--colored');
+    var icon = L.DomUtil.create('i', 'material-icons', button);
+    icon.textContent = 'add';
+    button.id = 'add-tile-server-button'
+
+    var dialog = document.querySelector('#add-tile-server-dialog');
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    button.addEventListener('click', function () {
+      dialog.showModal();
+    });
+    dialog.querySelector('.close').addEventListener('click', function () {
+      dialog.close();
+    });
+    dialog.querySelector('.add').addEventListener('click', function () {
+      var input = dialog.querySelector('#tile-server-link');
+      if (input.value && input.value.length > 0) {
+        addTileServer(input.value);
+      }
+      input.value = '';
+      dialog.close();
+    });
+    return button;
+  }
+});
+
+mymap.addControl(new AddButton());
 
 var addTileServer = function (url) {
   var tileServer = JSON.parse(localStorage.getItem('tileServer'));
-  if (!tileServer || typeof tileServer != 'Array') {
+  if (!tileServer || !tileServer instanceof Array) {
     tileServer = [];
   }
-  tileServer = [url].concat(tileServer);
+  if (tileServer.indexOf(url) != -1) {
+    return;
+  }
+  tileServer = [ url ].concat(tileServer);
   localStorage.setItem('tileServer', JSON.stringify(tileServer));
 }
